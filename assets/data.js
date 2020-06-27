18270c4da5a571ba8e83a7704348e9b1ad3bbb7b
redwanJemal <redjemal2@gmail.com>
2020-06-27 14:00:02 +0100

    Svg Icons added and Easytube UI Update

---
+++ assets/data.js
@@ -0,0 +1,55 @@
+        var menus = [
+            {
+                id: 1,
+                name: "Home"
+            },
+            {
+                id: 2,
+                name: "Services"
+            },
+            {
+                id: 3,
+                name: "Portfolio"
+            },
+            {
+                id: 4,
+                name: "Contuct us"
+            },
+            {
+                id: 5,
+                name: "About us"
+            }
+        ];
+
+        var products = [
+            {
+                id:"1",
+                name: "Product One",
+                img:"./kristy.png"
+            },
+            {
+                id:"2",
+                name: "Product Two",
+                img:"./labs.png"
+            },
+            {
+                id:"3",
+                name: "Product Three",
+                img:"./kristy.png"
+            },
+            {
+                id:"4",
+                name: "Product Four",
+                img:"./kristy.png"
+            },
+            {
+                id:"5",
+                name: "Product Five",
+                img:"./kristy.png"
+            },
+            {
+                id:"6",
+                name: "Product Six",
+                img:"./kristy.png"
+            }
+        ];
